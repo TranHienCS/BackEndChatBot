d21572d746311fa087bb796f89c8e492668f77e8
TranHien282 <you@example.com>
2019-08-23 15:32:11 +0700

    initial commit

---
+++ method/queryDB.js
@@ -0,0 +1,31 @@
+const Business = require('../model/trainingModel');
+
+module.exports={
+    async getAnswer(intent,entities){
+        return await Business.find({intent:intent,entities: entities},(err,docs)=>docs[0]);
+    },
+
+    getDefault_Answer(intent,param){
+        return Business.find({intent:intent,params:param},(err,docs)=>docs[0]);
+    },
+
+    deleteDocument(question){
+        Business.deleteOne({ question:question }, err => { if (err) console.log("err") });
+    },
+
+    async queryIntent(intent){
+        return await Business.find({intent: intent},(err,docs)=>docs)
+    },
+
+    async queryEntity(id){
+        let regex = new RegExp(('$'+id)+'|'+(id+','));
+        console.log(regex);
+        return await Business.find({"params":{$regex: regex , $options: 'i'}},(err,docs)=>docs);
+    },
+
+    async getAll(){
+        return await Business.find({},(err,docs)=>docs);
+    }
+    ///////////////////////////////////////////////////////
+    //////////////ELASTIC
+}
